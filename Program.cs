8da7595bc7db559f8c328ec78f36dfcf0da9e0a7
James Ross <git@james-ross.co.uk>
2022-01-22 22:52:27 +0000

    feat: Initial implementation of command-line parameters

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+
+class Program
+{
+    /// <summary>
+    /// A command-line tool for manipulating Microsoft To Do/Tasks
+    /// </summary>
+    /// <param name="config">Path to configuration file</param>
+    /// <param name="createTask">Action: create a new To Do/Task</param>
+    /// <param name="list">Specify the name or ID of a task list</param>
+    /// <param name="title">Specify the title of a task</param>
+    /// <param name="description">Specify the description of a task</param>
+    static void Main(FileInfo? config = null, bool createTask = false, string? list = null, string? title = null, string? description = null)
+    {
+        if (config == null) config = new FileInfo("config.json");
+        if (createTask)
+        {
+            ArgumentNullException.ThrowIfNull(list);
+            ArgumentNullException.ThrowIfNull(title);
+            CreateTask(LoadConfiguration(config), list, title, description ?? "");
+        }
+        else
+        {
+            throw new InvalidOperationException($"No valid action specified");
+        }
+    }
+
+    static IConfigurationRoot LoadConfiguration(FileInfo config)
+    {
+        return new ConfigurationBuilder()
+            .AddJsonFile(config.FullName, true)
+            .Build();
+    }
+
+    static void CreateTask(IConfigurationRoot config, string list, string title, string description)
+    {
+        Console.WriteLine($"Create Task");
+        Console.WriteLine($"List:        {list}");
+        Console.WriteLine($"Title:       {title}");
+        Console.WriteLine($"Description: {description}");
+    }
+}
