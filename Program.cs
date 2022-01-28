36789fd207bcb72fb7bd3d3d4b5c066b6d862139
James Ross <git@james-ross.co.uk>
2022-01-28 21:29:06 +0000

    feat: Authenticate with Exchange and add actions to show lists and tasks

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Exchange.WebServices.Data;
+using Microsoft.Extensions.Configuration;
+using Task = System.Threading.Tasks.Task;
 
 class Program
 {
@@ -6,14 +8,25 @@ class Program
     /// A command-line tool for manipulating Microsoft To Do/Tasks
     /// </summary>
     /// <param name="config">Path to configuration file</param>
+    /// <param name="lists">Action: show all lists</param>
+    /// <param name="tasks">Action: show all To Dos/Tasks in a list</param>
     /// <param name="createTask">Action: create a new To Do/Task</param>
     /// <param name="list">Specify the name or ID of a task list</param>
     /// <param name="title">Specify the title of a task</param>
     /// <param name="description">Specify the description of a task</param>
-    static void Main(FileInfo? config = null, bool createTask = false, string? list = null, string? title = null, string? description = null)
+    static async Task Main(FileInfo? config = null, bool lists = false, bool tasks = false, bool createTask = false, string? list = null, string? title = null, string? description = null)
     {
         if (config == null) config = new FileInfo("config.json");
-        if (createTask)
+        if (lists)
+        {
+            await Lists(LoadConfiguration(config));
+        }
+        else if (tasks)
+        {
+            ArgumentNullException.ThrowIfNull(list);
+            await Tasks(LoadConfiguration(config), list);
+        }
+        else if (createTask)
         {
             ArgumentNullException.ThrowIfNull(list);
             ArgumentNullException.ThrowIfNull(title);
@@ -32,11 +45,59 @@ class Program
             .Build();
     }
 
+    static ExchangeService GetExchange(IConfigurationRoot config)
+    {
+        var service = new ExchangeService(ExchangeVersion.Exchange2016);
+        service.Credentials = new WebCredentials(config["username"], config["password"]);
+        service.AutodiscoverUrl(config["email"], redirectionUri => new Uri(redirectionUri).Scheme == "https");
+        return service;
+    }
+
+    static async Task Lists(IConfigurationRoot config)
+    {
+        var service = GetExchange(config);
+        var taskFolder = await Retry("get tasks folder", () => Folder.Bind(service, WellKnownFolderName.Tasks));
+        var lists = await Retry("get lists", () => taskFolder.FindFolders(new FolderView(1000)));
+        foreach (var list in lists)
+        {
+            Console.WriteLine(list.DisplayName);
+        }
+    }
+
+    static async Task Tasks(IConfigurationRoot config, string listName)
+    {
+        var service = GetExchange(config);
+        var taskFolder = await Retry("get tasks folder", () => Folder.Bind(service, WellKnownFolderName.Tasks));
+        var lists = await Retry("get list", () => taskFolder.FindFolders(new SearchFilter.ContainsSubstring(FolderSchema.DisplayName, listName), new FolderView(1)));
+        if (lists.TotalCount == 0) throw new InvalidDataException($"No list containing text: {listName}");
+        var tasks = await Retry("get tasks", () => lists.First().FindItems(new ItemView(1000)));
+        foreach (var task in tasks)
+        {
+            Console.WriteLine(task.Subject);
+        }
+    }
+
     static void CreateTask(IConfigurationRoot config, string list, string title, string description)
     {
-        Console.WriteLine($"Create Task");
-        Console.WriteLine($"List:        {list}");
-        Console.WriteLine($"Title:       {title}");
-        Console.WriteLine($"Description: {description}");
+        var service = GetExchange(config);
+        // TODO: Find list
+        // TODO: Find task
+        // TODO: Create task
+    }
+
+    static async Task<T> Retry<T>(string name, Func<Task<T>> action)
+    {
+        while (true)
+        {
+            try
+            {
+                return await action();
+            }
+            catch (ServerBusyException error)
+            {
+                Console.WriteLine($"Retry of {name} due to server busy (back off for {error.BackOffMilliseconds} ms)");
+                Thread.Sleep(error.BackOffMilliseconds);
+            }
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Microsoft Tasks CLI
 
 A command-line tool for manipulating Microsoft To Do/Tasks.
+
+## Usage
+
+```
+dotnet run -- [options]
+```
+
+## Options
+
+```
+  --config <config>            Path to configuration file []
+  --lists                      Action: show all lists [default: False]
+  --tasks                      Action: show all To Dos/Tasks in a list [default: False]
+  --create-task                Action: create a new To Do/Task [default: False]
+  --list <list>                Specify the name or ID of a task list []
+  --title <title>              Specify the title of a task []
+  --description <description>  Specify the description of a task []
+  --version                    Show version information
+  -?, -h, --help               Show help and usage information
+```
